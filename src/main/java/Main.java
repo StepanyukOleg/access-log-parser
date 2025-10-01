c7ea9496647e0e1e4783afa9f760d02db7771058
Oleg Stepanyuk <qa.stepanyuk@yandex.ru>
2025-10-01 19:52:28 +0300

    Изменена логика приложение. Теперь это мини-калькулятор

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длинна текста: " + text.length());
+        System.out.println("Программа расчёта суммы, разницы, произведения и частного по введенным числам \nВведите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+        int summ = firstNumber + secondNumber;
+        int razn=firstNumber - secondNumber;
+        int proizv=firstNumber * secondNumber;
+        double chastnoe= (double) firstNumber / secondNumber;
+        System.out.println("Сумма: " + summ);
+        System.out.println("Разница: " + razn);
+        System.out.println("Произведение: " + proizv);
+        System.out.println("Частное: " + chastnoe);
     }
 }
